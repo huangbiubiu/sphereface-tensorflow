45cf666f3f3e9f10e72ac524f484eacd31b68d38
Huang Yuheng <hyhlryf@gmail.com>
2018-11-03 11:31:23 +0800

    Prove the problem is not from training file. Work same as last commmit.

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@
 #mkdir -p ${model_path}
 #CUDA_VISIBLE_DEVICES=0 nohup python -u train_webface.py --dataset_path=/home/hyh/projects/benchmark/data/webface --log_dir=${model_path} --softmax_type=a-softmax --cnn_model=a-softmax --margin=10 --batch_size=512 --max_step=16000 --learning_rate=1e-5 --eval_path=/home/hyh/projects/benchmark/data/lfw --base_lambda=1000 >> ${model_path}/webface_train.out &
 
-MODEL_PATH=/home/hyh/projects/benchmark/saved_models/reproduce/webface_16kstep_20klba
+MODEL_PATH=/home/hyh/projects/benchmark/saved_models/reproduce/webface_16kstep_20klba_newtrainingfile
 mkdir -p ${MODEL_PATH}
 CUDA_VISIBLE_DEVICES=0 nohup python -u train_webface.py --dataset_path=/home/hyh/projects/benchmark/data/webface --log_dir=${MODEL_PATH} --softmax_type=a-softmax --cnn_model=a-softmax --margin=10 --batch_size=512 --max_step=24000 --learning_rate=1e-4 --eval_path=/home/hyh/projects/benchmark/data/lfw --base_lambda=10000 >> ${MODEL_PATH}/webface_train.out &
